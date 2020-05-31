bb1718208c2d65cc389b8a7bde2a0ce1ec092cb3
Rafael Abuawad Vivanco <rafael.abuawad@live.com>
2020-05-31 15:45:51 -0400

    First commit
    Initial app setup, basic packages installed

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config:
+    JWT_SECRET_KEY = 'Shell App Secre Key'
+    SECRET_KEY = 'Shell App Secre Key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
